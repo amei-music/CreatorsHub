3ca1ab506aae9574e7aaa5e96aef69d495694c89
Naoki Yasuraoka <yasuraok@gmail.com>
2015-09-27 16:28:52 +0000

    Connect between MIDI and WebSocket-JSON

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ var http     = require('http');
 var socketio = require("socket.io")
 var midi     = require('midi');
 
+var LISTEN_PORT = 8080;
+
 //==============================================================================
 // 全体の管理情報
 //==============================================================================
+function Client(socket){
+  return {
+    socket: socket,
+  };
+}
+
 var self = {
   clients: {}, // socket.id: name
 
@@ -24,35 +32,53 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200, {"Content-Type":"text/html"});
   var output = fs.readFileSync("./index.html", "utf-8"); // カレントディレクトリのindex.htmlを配布
   res.end(output);
-}).listen(8080);
+}).listen(LISTEN_PORT);
 
 // websocketとしてlistenして、応答内容を記述
 var io = socketio.listen(server);
 io.sockets.on("connection", function (socket) {
-  console.log("listening connections...")
+  // (1) ただweb設定画面を見に来た人と、
+  // (2) WebSocket-JSON (以下wsjson) でネットワークに参加しにきた人と、
+  // (3) OSCでネットワークに参加しにきた人は別扱いする必要がある
 
-  // 接続開始カスタムイベント(接続元ユーザを保存し、他ユーザへ通知)
-  socket.on("connected", function (name) {
-    console.log("client '" + name + "' connected.");
-    self.clients[socket.id] = name;
-    // io.sockets.emit("publish", {value: msg});
+  // (1)のためのAPI
+  //  - ネットワーク接続者一覧を表示する
+  //  - ネットワークのノード間の接続/切断をする
+
+  // (2)のためのAPIは、(1)に加えて
+  //  - wsjsonクライアントとしてネットワークに参加する
+  //  - ネットワークから離脱する
+  //  - メッセージを送信する
+  // が必要。これらを関数化したjavascriptを配布する必要があるかも
+
+  // (3)のためのAPIは、(1)に加えて
+  //  - 指定のアドレス/ポート番号をoscクライアントとしてネットワークに追加する
+  //  - 指定のアドレス/ポート番号のoscクライアントをネットワークから除外する
+  // が必要。oscアプリ本体とこのserver.jsのoscモジュールが直接メッセージをやり取りするので、
+  // oscクライアントとの実通信にWebSocketは絡まない。あくまでコネクション管理のみ
+
+  // 接続開始
+  socket.on("connected", function (obj) {
+    console.log("client '" + obj.name + "' connected.");
+    self.clients[socket.id] = Client(socket, obj.name)
   });
 
-  // 接続終了組み込みイベント(接続元ユーザを削除し、他ユーザへ通知)
+  socket.on("publish", function (obj) {
+    // broadcast all clients (including the sender)
+    io.sockets.emit("publish", obj);
+  });
+
+  // 接続終了
   socket.on("disconnect", function () {
     if (self.clients[socket.id]) {
-      var name = self.clients[socket.id];
-      console.log("client '" + name + "' disconnected.");
+      console.log("client '" + self.clients[socket.id].name + "' disconnected.");
       delete self.clients[socket.id];
-      // io.sockets.emit("publish", {value: msg});
     }
   });
 
-  // メッセージ送信カスタムイベント
-  socket.on("publish", function (data) {
-    io.sockets.emit("publish", {value: data.value});
-  });
 });
+
+console.log("listening connections...")
 
 //==============================================================================
 // MIDIの設定
@@ -81,6 +107,9 @@ var midiObj = {
       // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
       // information interpreting the messages.
       console.log('m:' + message + ' d:' + deltaTime);
+
+      // broadcast all clients (including the sender)
+      io.sockets.emit("publish", {value: 'm:' + message + ' d:' + deltaTime});
     });
 
     // Open the first available input port.

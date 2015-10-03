b8594c0fbf124184693a8e8df8b5c06baf865924
Naoki Yasuraoka <yasuraok@gmail.com>
2015-10-04 00:52:46 +0900

    Refactor

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ var convert     = require('./convert')
 var LISTEN_PORT = 16080;
 var PUBLIC_DIR  = __dirname + "/public"
 
-oscSender = dgram.createSocket("udp4")
-
 //==============================================================================
 // 全体の管理情報
 //==============================================================================
@@ -22,7 +20,7 @@ function ClientJson(/*direction,*/ socketId){
     socketId:  socketId,
 
     deliver: function(msg, msg_from){
-      io.to(this.socketId).emit("message_json", convert.convertMessage(msg, msg_from, "json"));
+      g_io.to(this.socketId).emit("message_json", convert.convertMessage(msg, msg_from, "json"));
     },
 
     simplify: function(){ return {type: "json", socketId: this.socketId} },
@@ -41,7 +39,7 @@ function ClientOsc(/*direction,*/ host, port){
       console.log(buf)
       console.log(buf.length)
       console.log(this.port, this.host)
-      oscSender.send(buf, 0, buf.length, this.port, this.host);
+      g_oscSender.send(buf, 0, buf.length, this.port, this.host);
     },
 
     simplify: function(){ return {type: "osc", host: this.host, port: this.port} },
@@ -58,7 +56,7 @@ function ClientMidi(/*direction,*/ portNum, name){
     deliver: function(msg, msg_from){
       var buf = convert.convertMessage(msg, msg_from, "midi")
       console.log("midi out ", msg.address, " => ", buf)
-      midiObj.outputs[this.portNum].sendMessage(buf);
+      g_midiObj.outputs[this.portNum].sendMessage(buf);
     },
 
     simplify: function(){ return {type: "midi", portNum: this.portNum, name: this.name} },
@@ -66,32 +64,33 @@ function ClientMidi(/*direction,*/ portNum, name){
 }
 
 //==============================================================================
-// データ全体
+// プロトコルに依存しないクライアント一覧, コネクション設定に関する部分
 //==============================================================================
-var self = {
-  clients_input:  {},
-  clients_output: {},
-  id_input:      0,
-  id_output:     0, // 接続のユニークID
+function Clients(){ return {
+  inputs:      {},
+  outputs:     {},
+  connections: {}, // {input_clientId: {output_clientId: true, ...}, ...}
+  id_input:    0, // 接続のユニークID
+  id_output:   0, // 接続のユニークID
 
   //==============================================================================
   // クライアント管理
   //==============================================================================
   addNewClientInput: function(client){
-    this.clients_input[this.id_input] = client;
+    this.inputs[this.id_input] = client;
     this.id_input += 1;
     return this.id_input - 1;
   },
 
   addNewClientOutput: function(client){
-    this.clients_output[this.id_output] = client;
+    this.outputs[this.id_output] = client;
     this.id_output += 1;
     return this.id_output - 1;
   },
 
   deleteClientInput: function(clientId){
-    var existed = (clientId in this.clients_input);
-    delete this.clients_input[clientId];
+    var existed = (clientId in this.inputs);
+    delete this.inputs[clientId];
 
     for(var outputId in this.connections[clientId]){
       this.deleteConnection(clientId, outputId);
@@ -102,8 +101,8 @@ var self = {
   },
 
   deleteClientOutput: function(clientId){
-    var existed = (clientId in this.clients_output);
-    delete this.clients_output[clientId];
+    var existed = (clientId in this.outputs);
+    delete this.outputs[clientId];
 
     for(var inputId in this.connections){
       for(var outputId in this.connections[inputId]){
@@ -118,9 +117,6 @@ var self = {
   //==============================================================================
   // コネクション管理
   //==============================================================================
-  connections: {}, // {input_clientId: {output_clientId: true, ...}, ...}
-  oscsocks:    {}, // {input_oscport: {clientId: , sock: }} osc送受信オブジェクトを詰めておくところ
-  // socketはio.socketsで参照可能
 
   addConnection: function(input_clientId, output_clientId){
     // 結線情報を作る
@@ -145,16 +141,16 @@ var self = {
   //==============================================================================
   // inputに届いたメッセージをコネクション先に配信
   deliver: function(input_clientId, functor){
-    for(var outputId in self.connections[input_clientId]){
-      var output   = self.clients_output[outputId];
+    for(var outputId in this.connections[input_clientId]){
+      var output   = this.outputs[outputId];
       functor(output);
     }
   },
 
   // socketのcallbackで届いてきたメッセージの送信元を調べる
-  socketId2clientId: function(socketId, clients){
-    for (var k in clients) {
-      var client = clients[k];
+  socketId2clientId: function(socketId, inputsOutputs){
+    for (var k in inputsOutputs) {
+      var client = inputsOutputs[k];
       if(client.type == "json" && client.socketId == socketId){
         return k;
       }
@@ -162,94 +158,78 @@ var self = {
     return -1;
   },
 
-}
+}}
 
 
 //==============================================================================
-// WebSocketの設定
+// アプリ本体 (WebSocketの通信定義を作ったりMIDI/OSCの送受信を張ったり)
 //==============================================================================
+function App(){ return{
+  clients : Clients(),
+  oscsocks: {}, // {input_oscport: {clientId: , sock: }} osc送受信オブジェクトを詰めておくところ
+                // socket一覧はio.socketsにある
 
-// (1) ただweb設定画面を見に来た人と、
-// (2) WebSocket-JSON (以下wsjson) でネットワークに参加しにきた人と、
-// (3) OSCでネットワークに参加しにきた人は別扱いする必要がある
+  // ネットワーク接続者一覧を表示する(socketだからサーバー側からpush可能)
+  update_list : function(){
+    // メソッド類は削ぎ落として表示に必要な情報だけまとめる
+    var inputs  = {}; for (var i in this.clients.inputs ) inputs [i] = this.clients.inputs [i].simplify();
+    var outputs = {}; for (var o in this.clients.outputs) outputs[o] = this.clients.outputs[o].simplify();
 
-// (1)のためのAPI
-//  - ネットワーク接続者一覧を表示する(socketだからサーバー側からpush可能)
-function update_list(){
-  // メソッド類は削ぎ落として表示に必要な情報だけまとめる
-  var inputs  = {}; for (var i in self.clients_input ) inputs [i] = self.clients_input [i].simplify();
-  var outputs = {}; for (var o in self.clients_output) outputs[o] = self.clients_output[o].simplify();
+    // broadcast all clients (including the sender)
+    g_io.sockets.emit("update_list", {inputs: inputs, outputs: outputs, connections: this.clients.connections});
+  },
 
-  // broadcast all clients (including the sender)
-  io.sockets.emit("update_list", {inputs: inputs, outputs: outputs, connections: self.connections});
-}
-
-// (2)のためのAPIは、(1)に加えて
-//  - wsjsonクライアントとしてネットワークに参加する
-function join_as_wsjson(socket) {
-  var inputId  = self.addNewClientInput (ClientJson(socket.id));
-  var outputId = self.addNewClientOutput(ClientJson(socket.id));
-
-  console.log("[Web Socket #'" + socket.id + "'] joined as JSON client [id=" + inputId + "]");
-
-  update_list(); // ネットワーク更新
-}
-
-//  - ネットワークから離脱する
-function exit_wsjson(socket) {
-  var existed = false;
-  existed |= self.deleteClientInput (self.socketId2clientId(socket.id, self.clients_input ));
-  existed |= self.deleteClientOutput(self.socketId2clientId(socket.id, self.clients_output));
-
-  if(existed){
-    console.log("[Web Socket #'" + socket.id + "'] exited.");
-  }
-
-  update_list(); // ネットワーク更新
-}
-
-
-// websocketとしての応答内容を記述
-function onWebSocket(socket){
-  update_list(); // websocket接続時に一度現状を送る
-
-  //  - ネットワークのノード間の接続/切断をする
-  socket.on("add_connection", function (obj) {
+  // ネットワークのノード間の接続/切断をする
+  add_connection : function(obj){
     var inputId = obj.inputId, outputId = obj.outputId, connect = obj.connect
 
     if (connect == true){
-      self.addConnection(inputId, outputId) // 接続
+      this.clients.addConnection(inputId, outputId) // 接続
       console.log("input '" + inputId + "' connected to output '" + outputId + "'");
     } else {
-      self.deleteConnection(inputId, outputId) // 切断
+      this.clients.deleteConnection(inputId, outputId) // 切断
       console.log("input '" + inputId + "' and output '" + outputId + "' disconnected");
     }
 
-    update_list(); // ネットワーク更新
-  });
+    this.update_list(); // ネットワーク更新
+  },
 
-  // (2)のためのAPIは、(1)に加えて
-  //  - wsjsonクライアントとしてネットワークに参加する
-  socket.on("join_as_wsjson", function () { join_as_wsjson(socket); });
+  // wsjsonクライアントとしてネットワークに参加する
+  join_as_wsjson : function(socket) {
+    var inputId  = this.clients.addNewClientInput (ClientJson(socket.id));
+    var outputId = this.clients.addNewClientOutput(ClientJson(socket.id));
 
-  //  - ネットワークから離脱する
-  socket.on("exit_wsjson",    function () { exit_wsjson(socket); });
+    console.log("[Web Socket #'" + socket.id + "'] joined as JSON client [id=" + inputId + "]");
 
-  //  - メッセージを受信する
-  socket.on("message_json", function (obj) {
-    var inputId  = self.socketId2clientId(socket.id, self.clients_input);
+    this.update_list(); // ネットワーク更新
+  },
+
+  // ネットワークから離脱する
+  exit_wsjson : function(socket) {
+    var existed = false;
+    existed |= this.clients.deleteClientInput (this.clients.socketId2clientId(socket.id, this.clients.inputs ));
+    existed |= this.clients.deleteClientOutput(this.clients.socketId2clientId(socket.id, this.clients.outputs));
+
+    if(existed){
+      console.log("[Web Socket #'" + socket.id + "'] exited.");
+    }
+
+    this.update_list(); // ネットワーク更新
+  },
+
+  // JSONメッセージを受信する
+  message_json : function(socket, obj){
+    var inputId  = this.clients.socketId2clientId(socket.id, this.clients.inputs);
 
     if (inputId >= 0) { // joinしたクライアントだけがメッセージのやり取りに参加できる
       console.log("message from input #" + inputId);
 
-      self.deliver(inputId, function(output){output.deliver(obj, "json");} ); // 配信
+      this.clients.deliver(inputId, function(output){output.deliver(obj, "json");} ); // 配信
     }
-  });
-  // が必要。これらを関数化したjavascriptを配布する必要があるかも
+  },
 
-  // (3)のためのAPIは、(1)に加えて
   //  - 指定のアドレス/ポート番号をoscクライアントとしてネットワークに追加する
-  socket.on("join_as_osc", function (obj) {
+  join_as_osc : function(obj) {
     var inPort = 12345; // 受信ポートは指定が無ければサーバーが独自に決める
 
     // 入り口と出口のudpポートを作成する
@@ -257,123 +237,128 @@ function onWebSocket(socket){
       return function(msg, rinfo) {
         console.log("message from input #" + inputId);
 
-        self.deliver(inputId, function(output){output.deliver(msg, "osc");} ); // 配信
-
+        this.clients.deliver(inputId, function(output){output.deliver(msg, "osc");} ); // 配信
       }
     }
 
     // socketのlistenに成功してからネットワークに登録したいので、idは先回りで受け取る
-    self.oscsocks[inPort] = dgram.createSocket("udp4", _onRead(self.id_input));
-    self.oscsocks[inPort].bind(inPort);
+    this.oscsocks[inPort] = dgram.createSocket("udp4", _onRead(this.clients.id_input));
+    this.oscsocks[inPort].bind(inPort);
 
     // 接続ネットワークに参加する
-    var inputId  = self.addNewClientInput (ClientOsc(obj.host, inPort));
-    var outputId = self.addNewClientOutput(ClientOsc(obj.host, obj.port));
+    var inputId  = this.clients.addNewClientInput (ClientOsc(obj.host, inPort));
+    var outputId = this.clients.addNewClientOutput(ClientOsc(obj.host, obj.port));
 
     console.log("[OSC #'" + obj.host + "'] joined as OSC client [id=" + inputId + "]");
 
-    update_list(); // クライアントのネットワーク表示更新
-  });
-  //  - 指定のアドレス/ポート番号のoscクライアントをネットワークから除外する
-  // が必要。oscアプリ本体とこのserver.jsのoscモジュールが直接メッセージをやり取りするので、
-  // oscクライアントとの実通信にWebSocketは絡まない。あくまでコネクション管理のみ
+    this.update_list(); // クライアントのネットワーク表示更新
+  },
 
-  // ソケット自体の接続終了
-  socket.on("disconnect", function () {
-    exit_wsjson(socket); // 切断
-  });
+  // websocketとしての応答内容を記述
+  onWebSocket : function(socket){
+    this.update_list(); // websocket接続時に一度現状を送る
 
-}
+    // (1) ただweb設定画面を見に来た人と、
+    // (2) WebSocket-JSON (以下wsjson) でネットワークに参加しにきた人と、
+    // (3) OSCでネットワークに参加しにきた人は別扱いする必要がある
+
+    // (1)のためのAPI
+    socket.on("add_connection", this.add_connection.bind(this) );
+
+    // (2)のためのAPI
+    socket.on("join_as_wsjson", this.join_as_wsjson.bind(this, socket) ); // wsjsonクライアントとしてネットワークに参加する
+    socket.on("exit_wsjson",    this.exit_wsjson.bind(this, socket) );    // ネットワークから離脱する
+    socket.on("message_json",   this.message_json.bind(this, socket) );   // JSONメッセージを受信する
+
+    // (3)のためのAPI
+    socket.on("join_as_osc",    this.join_as_osc.bind(this) );              // 指定のアドレス/ポート番号をoscクライアントとしてネットワークに追加する
+    socket.on("exit_osc",       function(){ console.log("unimplemented")}); // 指定のアドレス/ポート番号をoscクライアントとしてネットワークから除外する
+    // oscアプリ本体とこのserver.jsのoscモジュールが直接メッセージをやり取りするので、
+    // oscクライアントとの実通信にWebSocketは絡まない。あくまでコネクション管理のみ
+
+    // ソケット自体の接続終了
+    socket.on("disconnect",     this.exit_wsjson.bind(this, socket) );
+  },
+
+  // 新規MIDI入力デバイスの登録
+  newMidiInput : function(portId, name){
+    // ネットワークに登録
+    console.log("input  ", portId, name);
+    var inputId  = this.clients.addNewClientInput (ClientMidi(portId, name));
+
+    // コールバックを作る(影でinputIdをキャプチャする)
+    return function(deltaTime, message) {
+      // The message is an array of numbers corresponding to the MIDI bytes:
+      //   [status, data1, data2]
+      // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
+      // information interpreting the messages.
+      var msg = {'msg': message, 'delta': deltaTime};
+      console.log(msg);
+
+      this.clients.deliver(inputId, function(output){output.deliver(message, "midi");} ); // 配信
+    };
+  },
+
+  // 新規MIDI出力デバイスの登録
+  newMidiOutput : function(portId, name){
+    // ネットワークに登録
+    console.log("output ", portId, name);
+    var outputId = this.clients.addNewClientOutput(ClientMidi(portId, name));
+  },
+}}
 
 //==============================================================================
-// MIDIの設定
+// MIDIリストを管理してくれる人
 //==============================================================================
-
-// Set up a new input.
-var midiObj = {
+function MidiObj(){ return {
   input:  new midi.input(),  // port一覧を出すためのglobalな(openPortしない)inputを一つ用意しておく
   output: new midi.output(), // port一覧を出すためのglobalな(openPortしない)outputを一つ用意しておく
 
   inputs : {}, // 開いたportにつながっているmidiオブジェクト
   outputs: {}, // 開いたportにつながっているmidiオブジェクト
 
-  setup_midiports: function(){
-    // inputをopen
+  setup_midiports: function(onNewInput, onNewOutput){
     for(var portId=0; portId<this.input.getPortCount(); ++portId){
-      console.log("input  ", portId, this.input.getPortName(portId));
-
-      // ネットワークに登録
-      var inputId  = self.addNewClientInput (ClientMidi(portId, this.input.getPortName(portId)));
-
-      // コールバックを作る(inputIdをキャプチャする)
-      var _onMessage = function(_inputId){
-        return function(deltaTime, message) {
-          // The message is an array of numbers corresponding to the MIDI bytes:
-          //   [status, data1, data2]
-          // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
-          // information interpreting the messages.
-          var msg = {'msg': message, 'delta': deltaTime};
-          console.log(msg);
-
-          self.deliver(_inputId, function(output){output.deliver(message, "midi");} ); // 配信
-        };
-      }
-
-      // 開く
-      this.inputs[portId] = this.openInput(portId, _onMessage(inputId));
+      // inputを見つけたときの処理を実行し、そのinput MIDI portからの受信時にしたい処理をもらう
+      var callback = onNewInput(portId, this.input.getPortName(portId));
+      // 実際に開いておく
+      this.inputs[portId] = new midi.input();
+      this.inputs[portId].on('message', callback);
+      this.inputs[portId].openPort(portId);
+      // (Sysex, Timing, Active Sensing) のignoreを設定する
+      this.inputs[portId].ignoreTypes(false, false, true);
     }
 
-    // outputをopen
     for(var portId=0; portId<this.output.getPortCount(); ++portId){
-      console.log("output ", portId, this.output.getPortName(portId));
-
-      // ネットワークに登録
-      var outputId = self.addNewClientOutput(ClientMidi(portId, this.output.getPortName(portId)));
-
-      // 開く
+      // outputを見つけたときの処理を実行する
+      onNewOutput(portId, this.output.getPortName(portId));
+      // 実際に開いておく
       this.outputs[portId] = new midi.output();
       this.outputs[portId].openPort(portId);
     }
-
-    return;
   },
-
-  openInput: function(port, callback){
-    var this_input  = new midi.input();
-
-    this_input.on('message', callback); // configure a callback.
-    this_input.openPort(port);          // open port
-
-    // Sysex, timing, and active sensing messages are ignored
-    // by default. To enable these message types, pass false for
-    // the appropriate type in the function below.
-    // Order: (Sysex, Timing, Active Sensing)
-    // For example if you want to receive only MIDI Clock beats
-    // you should use
-    // input.ignoreTypes(true, false, true)
-    this_input.ignoreTypes(false, false, true);
-
-    return this_input;
-  }
-
-}
+}}
 
 
 //==============================================================================
 // start!
 //==============================================================================
+var g_app       = App();
+var g_midiObj   = MidiObj();
+var g_oscSender = dgram.createSocket("udp4")
+
 // midiのコネクションを作成
-midiObj.setup_midiports();
+g_midiObj.setup_midiports(g_app.newMidiInput.bind(g_app), g_app.newMidiOutput.bind(g_app));
 
 // PUBLIC_DIR以下を普通のhttpサーバーとしてlisten
-var app = connect();
-app.use(serveStatic(PUBLIC_DIR));
-var server = http.createServer(app);
-server.listen(LISTEN_PORT);
+var g_httpApp = connect();
+g_httpApp.use(serveStatic(PUBLIC_DIR));
+var g_server = http.createServer(g_httpApp);
+g_server.listen(LISTEN_PORT);
 
 // websocketとしてlistenして、応答内容を記述
-var io = socketio.listen(server);
-io.sockets.on("connection", onWebSocket);
+var g_io = socketio.listen(g_server);
+g_io.sockets.on("connection", g_app.onWebSocket.bind(g_app));
 
 console.log("================================================");
 console.log("listening web socket on port " + LISTEN_PORT);

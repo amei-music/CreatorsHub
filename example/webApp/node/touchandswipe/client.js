1ee520817da6a1b9db451b0ea7fc0c7efa7786b6
usuijun <usui@usuimac.local>
2016-06-03 02:22:21 +0900

    [WIP] update

--- example/webApp/node/touchandswipe/client.js
+++ example/webApp/node/touchandswipe/client.js
@@ -1,16 +1,16 @@
+/**
+* 各種マウスイベント、タッチイベントを取得していますが、
+* 現状ではtouchstart, toucnendにのみ反応します
+* つまりPCのブラウザからは動作しません
+*/
+
 var gSocket;
 var canvas;
 var isMouseDown = false;
 
 function init() {
-  console.log('Connecting to server');
-  canvas = document.getElementById('myCanvas');
-  var ctx = canvas.getContext('2d');
-
-  // fill canvas with gray
-  ctx.fillStyle = 'rgb(200,200,200)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  gSocket = io.connect();
+  // set canvas size to full screen
+  resize();
 
   // socket connect and disconnect callback
   gSocket.on( "connect", function() {
@@ -25,7 +25,8 @@ function init() {
     console.log("mouse down");
     console.log(e);
     isMouseDown = true;
-    onDown(e);
+    var mouseID = 0; // マウスは一点しかクリック出来ないので0固定
+    //onDown(e, mouseID);
  }, false);
 
  canvas.addEventListener('mousemove', function(e) {
@@ -38,29 +39,70 @@ function init() {
   canvas.addEventListener('mouseup', function(e) {
     console.log("mouse up");
     isMouseDown = false;
-    onUp(e);
+    var mouseID = 0; // マウスは一点しかクリック出来ないので0固定
+    //onUp(e, mouseID);
   }, false);
  canvas.addEventListener('touchstart', function(e) {
    console.log("touch start");
    console.log(e);
-    onDown(e);
+   for (var i = 0; i < event.changedTouches.length; i++) {
+     var touch = event.changedTouches[i];
+     console.log(touch.pageX + "," + touch.pageY);
+     onDown(touch, touch.identifier);
+   }
   }, false);
  canvas.addEventListener('touchmove', function(e) {
-   for (var i = 0; i < event.touches.length; i++) {
-     var touch = event.touches[i];
+   for (var i = 0; i < event.changedTouches.length; i++) {
+     var touch = event.changedTouches[i];
      console.log(touch.pageX + "," + touch.pageY);
      onMove(touch);
    }
   }, false);
- canvas.addEventListener('touchend', onUp, false);
+ canvas.addEventListener('touchend', function(e) {
+   for (var i = 0; i < event.changedTouches.length; i++) {
+     var touch = event.changedTouches[i];
+     console.log(touch.pageX + "," + touch.pageY);
+     onUp(touch, touch.identifier);
+   }
+ }, false);
 }
 
-function onDown(e) {
+function resize() {
+  $('#myCanvas').get( 0 ).width = $( window ).width();
+  $('#myCanvas').get( 0 ).height = $( window ).height();
+  console.log('Connecting to server');
+  canvas = document.getElementById('myCanvas');
+  var ctx = canvas.getContext('2d');
+
+  // fill canvas with gray
+  ctx.fillStyle = 'rgb(200,200,200)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  gSocket = io.connect();
+
+  // set title
+  ctx.fillStyle='#ffffff';
+  ctx.font='26px Arial';
+  ctx.textAlign='left';
+  ctx.fillText("Note On by Tap", 10, 32);
+  ctx.fillText("left: low note --- high note : right", 10, 64);
+  ctx.fillText("up: low velocity --- high velocity : down", 10, 96);
+}
+
+function onDown(e, id) {
   console.log("on down");
-  console.log(e.pageX);
+  console.log(e);
   x = e.pageX;
   y = e.pageY;
-  sendXY(x, y);
+  sendDown(x, y, id);
+
+  var canvas = document.getElementById('myCanvas');
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'rgb(200,200,200)';
+  ctx.fillRect(10,102-12,100,12);
+  ctx.fillStyle='#ffffff';
+  ctx.font='10px Arial';
+  ctx.textAlign='left';
+  ctx.fillText("down id:"+id, 10, 102);
 }
 
 function onMove(e) {
@@ -68,17 +110,39 @@ function onMove(e) {
   console.log(e.pageX + "," + e.pageY);
 }
 
-function onUp(e) {
+function onUp(e, id) {
   console.log("on up");
+  console.log("on down");
+  console.log(e);
+  x = e.pageX;
+  y = e.pageY;
+  sendUp(x, y, id);
+
+  var canvas = document.getElementById('myCanvas');
+  var ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'rgb(200,200,200)';
+  ctx.fillRect(10,122-12,100,12);
+  ctx.fillStyle='#ffffff';
+  ctx.font='10px Arial';
+  ctx.textAlign='left';
+  ctx.fillText("up id:"+id, 10, 122);
 }
 
-function sendXY(x, y) {
-  var rect = canvas.getBoundingClientRect() ;
-  canvasx = rect.left + window.pageXOffset;
-  canvasy = rect.top + window.pageYOffset;
+function sendDown(x, y, id) {
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  x = x / windowWidth;
+  y = y / windowHeight;
+  gSocket.emit("down", {x:x, y:y, id:id});
+}
 
-  x = x / (canvasx + canvas.width);
-  y = y / (canvasy + canvas.height);
-
-  gSocket.emit("xyval", {x:x, y:y});
+function sendUp(x, y, id) {
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
+  x = x / windowWidth;
+  y = y / windowHeight;
+  gSocket.emit("up", {x:x, y:y, id:id});
+}
+function clamp(v, min, max) {
+  return Math.min(Math.max(v, min), max);
 }

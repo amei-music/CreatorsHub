a5fe83c028c6a63fbbc4c7ae120dd962a2c3133f
Motoji Nagata <nagata@uniq.jp>
2016-06-28 16:54:08 +0900

    Fixed: [can not use jQuery/RequireJS/Meteor/AngularJS in Electron.] http://electron.atom.io/docs/faq/#i-can-not-use-jqueryrequirejsmeteorangularjs-in-electron

--- prg/index.js
+++ prg/index.js
@@ -14,7 +14,17 @@ var server = require('./server');
 var mainWindow = null;
 app.on('ready', function() {
   // ブラウザウィンドウを作成
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    // http://electron.atom.io/docs/faq/#i-can-not-use-jqueryrequirejsmeteorangularjs-in-electron
+    // [I can not use jQuery/RequireJS/Meteor/AngularJS in Electron.]
+    // Due to the Node.js integration of Electron, there are some extra symbols inserted into the DOM like module, exports, require. This causes problems for some libraries since they want to insert the symbols with the same names.
+    // To solve this, you can turn off node integration in Electron:
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
 
   // 設定画面をロード
   mainWindow.loadURL('http://localhost:16080');
